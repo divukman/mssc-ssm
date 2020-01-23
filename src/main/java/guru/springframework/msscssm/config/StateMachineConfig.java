3173d25af3d18817aa1035f7ee5d03109e08d8bd
divukman <divukman@gmail.com>
2020-01-23 15:43:27 +0100

    Refactoring

--- src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -1,12 +1,12 @@
 package guru.springframework.msscssm.config;
 
+import guru.springframework.msscssm.config.actions.*;
+import guru.springframework.msscssm.config.guards.PaymentIdGuard;
 import guru.springframework.msscssm.domain.PaymentEvent;
 import guru.springframework.msscssm.domain.PaymentState;
-import guru.springframework.msscssm.services.PaymentServiceImpl;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
@@ -16,12 +16,20 @@ import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
 import java.util.EnumSet;
-import java.util.Random;
 
 @Slf4j
 @EnableStateMachineFactory
 @Configuration
+@RequiredArgsConstructor
 public class StateMachineConfig extends StateMachineConfigurerAdapter <PaymentState, PaymentEvent> {
+
+    private final PaymentIdGuard paymentIdGuard;
+    private final PreAuthAction preAuthAction;
+    private final AuthAction authAction;
+    private final PreAuthApprovedAction preAuthApprovedAction;
+    private final PreAuthDeclinedAction preAuthDeclinedAction;
+    private final AuthApprovedAction authApprovedAction;
+    private final AuthDeclinedAction authDeclinedAction;
 
     @Override
     public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
@@ -37,19 +45,23 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter <PaymentSt
     public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
         transitions
                 .withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
-                .action(preAuthAction())
+                    .action(preAuthAction).guard(paymentIdGuard)
                 .and()
                 .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
+                 .action(preAuthApprovedAction)
                 .and()
                 .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED)
+                 .action(preAuthDeclinedAction)
                 //preauth to auth
                 .and()
                 .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.PRE_AUTH).event(PaymentEvent.AUTHORIZE)
-                .action(authAction())
+                 .action(authAction)
                 .and()
                 .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH).event(PaymentEvent.AUTH_APPROVED)
+                  .action(authApprovedAction)
                 .and()
-                .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH_ERROR).event(PaymentEvent.AUTH_DECLINED);
+                .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH_ERROR).event(PaymentEvent.AUTH_DECLINED)
+                  .action(authDeclinedAction);
     }
 
 
@@ -65,7 +77,7 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter <PaymentSt
         config.withConfiguration().listener(stateMachineListenerAdapter);
     }
 
-    public Action<PaymentState, PaymentEvent> preAuthAction() {
+/*    public Action<PaymentState, PaymentEvent> preAuthAction() {
         return stateContext -> {
             System.out.println("PreAuth called...");
 
@@ -85,9 +97,9 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter <PaymentSt
                 );
             }
         };
-    }
+    }*/
 
-    public Action<PaymentState, PaymentEvent> authAction(){
+/*    public Action<PaymentState, PaymentEvent> authAction(){
         return context -> {
             System.out.println("Auth was called!!!");
 
@@ -104,5 +116,12 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter <PaymentSt
                         .build());
             }
         };
-    }
+    }*/
+
+
+/*    public Guard<PaymentState, PaymentEvent> paymentIdGuard () {
+        return context -> {
+            return context.getMessageHeader(PaymentServiceImpl.PAYMENT_ID_HEADER) != null;
+        };
+    }*/
 }
